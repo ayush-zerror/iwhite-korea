ce7ca39b8e4d8c8cfcefc0736671dfb2dca578a3
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2024-10-28 18:52:38 +0530

    button anime

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -2,28 +2,28 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import React from 'react'
 
-const Section1 = ({title ,vid}) => {
-  useGSAP(()=>{
-    gsap.from(".hometxt",{
-      y:20,
-      opacity:0,
-      duration:1,
-      stagger:.2
+const Section1 = ({ title, vid }) => {
+  useGSAP(() => {
+    gsap.from(".hometxt", {
+      y: 20,
+      opacity: 0,
+      duration: 1,
+      stagger: .2
     })
-  },[])
+  }, [])
   return (
     <div className='fixed top-0 left-0 w-full h-screen flex'>
-      <video autoPlay muted loop  className='w-full  h-full object-cover' src={vid}></video>
+      <video autoPlay muted loop className='w-full  h-full object-cover' src={vid}></video>
       <div className='w-full h-full absolute top-0 left-0 bg-gradient-to-b from-[#0000000c] to-[#000000c2]  text-white flex flex-col items-center justify-end pb-[30vw] sm:pb-[6vw]'>
         {
           title && (<>
-          <h2 className='hometxt text-[9vw] sm:text-[3vw] font-[third] mb-2 sm:mb-0 leading-none text-center'>Your First In Korean Skincare.</h2>
-        <h2 className='hometxt text-[9vw] sm:text-[3vw] font-[third] leading-none text-center'>Your First Love.</h2>
-        <p className='hometxt font-[second] my-[6vw] sm:my-[1vw]'>For hydration that never clocks off. </p>
-        <div className='hometxt capitalize text-white px-8 py-2  border border-white rounded-full hover:bg-zinc-800 cursor-pointer hover:border-zinc-800 relative overflow-hidden'>
-          <div className='w-full h-full bg-black absolute top-full left-1/2 -translate-x-1/2 rounded-[50%]'></div>
-          <h2 className='relative whitespace-nowrap'>find out more</h2>
-        </div>
+            <h2 className='hometxt text-[9vw] sm:text-[3vw] font-[third] mb-2 sm:mb-0 leading-none text-center'>Your First In Korean Skincare.</h2>
+            <h2 className='hometxt text-[9vw] sm:text-[3vw] font-[third] leading-none text-center'>Your First Love.</h2>
+            <p className='hometxt font-[second] my-[6vw] sm:my-[1vw]'>For hydration that never clocks off. </p>
+            <div className='btn-anime capitalize text-white px-8 py-2  border border-white rounded-full cursor-pointer relative overflow-hidden'>
+              <div className='curve  transition-all duration-500 w-full h-full bg-white absolute top-full left-1/2 -translate-x-1/2 rounded-[50%]'></div>
+              <h2 className='txtbtn relative whitespace-nowrap transition-all duration-500 '>find out more</h2>
+            </div>
           </>)
         }
       </div>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,60 +1,61 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Section4 = () => {
-  return (
-    <div className='w-full  pt-[15vw] sm:pt-[5vw] bg-[#FDF9EE] pointer-events-auto'>
-        <div className='w-full  flex items-center justify-between px-3 sm:px-6 '>
-            <h2 className='text-[6vw] sm:text-[2.5vw] font-[third]'>@iwhitekorea</h2>
-            <div className='hometxt capitalize text-black px-8 py-2  border border-black rounded-full cursor-pointer relative overflow-hidden'>
-          <div className='w-full h-full bg-black absolute top-full left-1/2 -translate-x-1/2 rounded-[50%]'></div>
-          <h2 className='relative whitespace-nowrap'>follow us</h2>
-        </div>
-        </div>
-        <div className='w-full py-[8vw] sm:py-[5vw] overflow-hidden flex'>
-            <div className='socal-mq w-fit flex items-center gap-4 pr-4'>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post1.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post3.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post4.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post5.jpg" alt="" />
+    return (
+        <div className='w-full  pt-[15vw] sm:pt-[5vw] bg-[#FDF9EE] pointer-events-auto'>
+            <div className='w-full  flex items-center justify-between px-3 sm:px-6 '>
+                <Link href='https://www.instagram.com/iwhitekorea/' className='text-[6vw] sm:text-[2.5vw] font-[third]'>@iwhitekorea</Link>
+                <div className='btn-anime transition-all duration-500 capitalize text-black px-8 py-2  border border-black rounded-full cursor-pointer relative overflow-hidden'>
+                    <div className='curve  transition-all duration-500 w-full h-full bg-black absolute top-full left-1/2 -translate-x-1/2 rounded-[50%]'></div>
+                    <Link href='https://www.instagram.com/iwhitekorea/' className='relative whitespace-nowrap'>follow us</Link>
                 </div>
             </div>
-            <div className='socal-mq w-fit flex items-center gap-4 pr-4'>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post1.jpg" alt="" />
+            <div className='w-full py-[8vw] sm:py-[5vw] overflow-hidden flex'>
+                <div className='socal-mq w-fit flex items-center gap-4 pr-4'>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post1.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post3.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post4.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post5.jpg" alt="" />
+                    </div>
                 </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
+                <div className='socal-mq w-fit flex items-center gap-4 pr-4'>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post1.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post3.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post4.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
+                    </div>
+                    <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
+                        <img className='w-full h-full object-cover' src="/post5.jpg" alt="" />
+                    </div>
                 </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post3.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post4.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post2.jpg" alt="" />
-                </div>
-                <div className='w-[40vw] sm:w-[18vw] h-[40vw] sm:h-[18vw] shrink-0 rounded-xl overflow-hidden'>
-                    <img className='w-full h-full object-cover' src="/post5.jpg" alt="" />
-                </div>
+
             </div>
-           
         </div>
-    </div>
-  )
+    )
 }
 
 export default Section4
