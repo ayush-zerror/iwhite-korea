07b2923ac2589395eb053ecfced8d316738d06a3
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2024-11-25 18:18:36 +0530

    height card

--- src/components/home/Section3.jsx
+++ src/components/home/Section3.jsx
@@ -30,7 +30,7 @@ export default function Section3() {
                 {filterProducts.map((p,i)=>(
                     <SwiperSlide key={i}>
                     <Link href={`/products/${p.category}/${p.name}`}>
-                    <img className='h-[45vh] sm:h-[70vh] w-full object-cover' src={p.images[0]} alt="" />
+                    <img className='h-[42vh] sm:h-[70vh] w-full object-cover' src={p.images[0]} alt="" />
                     <h4 className='text-[4.2vw] sm:text-[1.2vw] font-[second] text-[#030303] mt-[1vw]'>{p.name}</h4>
                     <div className='flex items-center gap-1  text-[4.2vw] sm:text-[1.2vw] font-semibold text-black'>
                         <i className="ri-star-s-fill"></i><i className="ri-star-s-fill"></i><i className="ri-star-s-fill"></i><i className="ri-star-s-fill"></i><i className="ri-star-s-fill"></i>
